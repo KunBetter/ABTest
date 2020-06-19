c5a6557278767e0608f1724b67c592ab7456aee6
kunbetter <xinxi.li@dianping.com>
2020-06-19 10:19:47 +0800

    set bucket

--- core/experiment/DefaultExperimentManager.go
+++ core/experiment/DefaultExperimentManager.go
@@ -31,21 +31,7 @@ func (manager *DefaultExperimentManager) handlerExpGroup(config string) {
 		fmt.Println("some error")
 	}
 
-	start := 0
-	for i := 0; i < len(expGroup.Experiments); i++ {
-		exp := &expGroup.Experiments[i]
-		exp.setBuckets(start, start+exp.Traffic)
-		start += exp.Traffic
-
-		exp.WhiteSet = make(map[string]bool)
-		for i := 0; i < len(exp.Whitelist); i++ {
-			exp.WhiteSet[exp.Whitelist[i]] = true
-		}
-	}
-
-	if start > 100 {
-		//throw new Exception
-	}
-
-	manager.ExpGroupMap[expGroup.LayId] = []*ExperimentGroup{&expGroup}
+	ptr := &expGroup
+	ptr.SetBucket()
+	manager.ExpGroupMap[expGroup.LayId] = []*ExperimentGroup{ptr}
 }
--- core/experiment/ExperimentGroup.go
+++ core/experiment/ExperimentGroup.go
@@ -9,3 +9,21 @@ type ExperimentGroup struct {
 	Conditions   map[string]string `json:"conditions"`
 	Experiments  []Experiment      `json:"experiments"`
 }
+
+func (expGroup *ExperimentGroup) SetBucket() {
+	start := 0
+	for i := 0; i < len(expGroup.Experiments); i++ {
+		exp := &expGroup.Experiments[i]
+		exp.setBuckets(start, start+exp.Traffic)
+		start += exp.Traffic
+
+		exp.WhiteSet = make(map[string]bool)
+		for i := 0; i < len(exp.Whitelist); i++ {
+			exp.WhiteSet[exp.Whitelist[i]] = true
+		}
+	}
+
+	if start > 100 {
+		//throw new Exception
+	}
+}
